48e77c2aefbeefa74f94f0c529a09c68c6873b58
ElenaBer77 <pei281277@gmail.com>
2024-04-09 22:27:08 +0300

    parameterizedCsv

--- src/test/java/ru/netology/services/restMonth/services/RestMonthTest.java
+++ src/test/java/ru/netology/services/restMonth/services/RestMonthTest.java
@@ -2,30 +2,37 @@ package ru.netology.services.restMonth.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.services.restMonth.services.RestMonthService;
 
 public class RestMonthTest {
-    @Test
-    public void calculationOfVacationMonthsLow() {
+    @ParameterizedTest
+    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    })
+    public void calculationOfVacationMonthsLow(int expected, int income, int expense, int threshold) {
         RestMonthService service = new RestMonthService();
-        int expected = 3;
-        int income = 10_000; // доход
-        int expense = 3_000; // расход
-        int threshold = 20_000; // порог
+
+        //int expected = 3;
+        //   int income = 10_000; // доход
+        //  int expense = 3_000; // расход
+        // int threshold = 20_000; // порог
         int actual = service.calculator(income, expense, threshold);
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void calculationOfVacationMonthsHigh() {
-        RestMonthService service = new RestMonthService();
-        int expected = 2;
-        int income = 100_000; // доход
-        int expense = 60_000; // расход
-        int threshold = 150_000; // порог
-        int actual = service.calculator(income, expense, threshold);
+    //@Test
+    //public void calculationOfVacationMonthsHigh() {
+    //  RestMonthService service = new RestMonthService();
+    // int expected = 2;
+    // int income = 100_000; // доход
+    //int expense = 60_000; // расход
+    // int threshold = 150_000; // порог
+    //int actual = service.calculator(income, expense, threshold);
 
-        Assertions.assertEquals(expected, actual);
-    }
+    //Assertions.assertEquals(expected, actual);
+    // }
 
 }
